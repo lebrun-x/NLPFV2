0f21ed0435eb07a754c03ab705c22355187c4ae2
Steven Ghirighini <ghirig_s@epita.fr>
2017-01-09 21:00:00 +0100

    [ADDED] Page catch in controller

--- NLPF1/src/AppBundle/Controller/DefaultController.php
+++ NLPF1/src/AppBundle/Controller/DefaultController.php
@@ -8,14 +8,76 @@ use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
+
     /**
-     * @Route("/", name="homepage")
+     * index.html
+     * @Route("/")
      */
-    public function indexAction(Request $request)
+    public function indexhtml()
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
+        return $this->render('index.html');
     }
+
+    /**
+     * connexion.html
+     * @Route("/connexion")
+     */
+    public function connexionhtml()
+    {
+        // replace this example code with whatever you need
+        return $this->render('connexion.html');
+    }
+
+    /**
+     * best.html
+     * @Route("/best")
+     */
+    public function besthtml()
+    {
+        // replace this example code with whatever you need
+        return $this->render('best.html');
+    }
+
+    /**
+     * participate.html
+     * @Route("/participate")
+     */
+    public function participatehtml()
+    {
+        // replace this example code with whatever you need
+        return $this->render('participate.html');
+    }
+
+    /**
+     * project_details.html
+     * @Route("/project_details")
+     */
+    public function project_detailshtml()
+    {
+        // replace this example code with whatever you need
+        return $this->render('project_details.html');
+    }
+
+    /**
+     * projet.html
+     * @Route("/projet")
+     */
+    public function projethtml()
+    {
+        // replace this example code with whatever you need
+        return $this->render('projet.html');
+    }
+
+    /**
+     * user.html
+     * @Route("/user")
+     */
+    public function userhtml()
+    {
+        // replace this example code with whatever you need
+        return $this->render('user.html');
+    }
+
+
 }
