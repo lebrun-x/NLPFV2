77c0ced58eb0c8afdd56b02a326263368299b3e8
Steven Ghirighini <ghirig_s@epita.fr>
2017-01-12 02:36:06 +0100

    [ADDED] Buttons to draw project details, to participate to a specific compensation and to confirm participation

--- NLPF1/src/AppBundle/Controller/ParticipateController.php
+++ NLPF1/src/AppBundle/Controller/ParticipateController.php
@@ -41,4 +41,34 @@ class ParticipateController extends Controller
 
         return $this->render('participate.html.twig', array("project" => $project, "compensation" => $compensation));
     }
+
+    /**
+     * @Route("/participate/{pid}/{cid}/confirm")
+     */
+    public function confirm($pid, $cid)
+    {
+        /*** TODO: INSERT INTO \"contribution\" VALUES (default, now(), " + userId + ", " + compensationId + ") RETURNING contribution_id, date, ref_user_id, ref_compensation_id"; ***/
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $qb1 = $em->createQueryBuilder();
+
+        $qb1->select('c')
+            ->from('AppBundle:Compensation', 'c')
+            ->where('c.compensation_id = ' . $cid);
+
+        $query1 = $qb1->getQuery();
+
+        $compensation = $query1->getResult();
+
+        $qb2 = $em->createQueryBuilder();
+        $qb2->select('p')
+            ->from('AppBundle:Project', 'p')
+            ->where('p.project_id = ' . $pid);
+
+        $query2 = $qb2->getQuery();
+
+        $project = $query2->getResult();
+
+        return $this->render('index.html.twig', array("project" => $project, "compensation" => $compensation));
+    }
 }
